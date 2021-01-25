f9d5833bd4833a9952f5cff4467f7e163648e502
lqonkee81 <arsik.uskov@mail.ru>
2021-01-25 18:30:47 +0800

    25.01.10

--- VsCode/22.01.21/5.cpp
+++ VsCode/22.01.21/5.cpp
@@ -9,10 +9,10 @@ bool Check(const long Num)
 
     bool chk1 = true;
     bool chk2 = true;
-
+    int iter = 5;
     long StockNum = Num;
 
-    for (int i = 0; i < 3; ++i)
+    /* for (int i = 0; i < 3; ++i)
     {
         if ((static_cast<int>(strNum[i]) - static_cast<int>('0')) % 2 == 0)
             chk1 = true;
@@ -29,7 +29,29 @@ bool Check(const long Num)
     if ((chk1 == true) && (chk2 == true))
         return true;
     else
-        return false;
+        return false;*/
+
+    while (iter > 2)
+    {
+        if ((StockNum % 10) % 2 != 0)
+            chk1 = true;
+        else
+            return false;
+        StockNum /= 10;
+        iter--;
+    }
+
+    while (iter > 0)
+    {
+        if ((StockNum % 10) % 2 == 0)
+            chk1 = true;
+        else
+            return false;
+        StockNum /= 10;
+        iter--;
+    }
+
+    return chk1;
 }
 
 int main()
